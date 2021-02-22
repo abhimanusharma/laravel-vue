44964b84f879e526ec8839c9c45be611cc2a5a95
Abhimanu <abhimanu.sharma@softobiz.com>
2021-02-22 17:21:11 +0530

    basic vue setup

---
+++ resources/js/pages/Test.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+
+  <section class="resources-js-pages-test">
+    <h1>resources-js-pages-test Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'resources-js-pages-test',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="scss">
+  .resources-js-pages-test {
+
+  }
+</style>
